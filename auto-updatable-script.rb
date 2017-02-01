8564e4cc624a0a9aef56f40092dc04a966ef47d4
ariera <alejandro.riera@embo.org>
2017-02-01 09:26:50 +0100

    better structure

--- auto-updatable-script.rb
+++ auto-updatable-script.rb
@@ -1,24 +1,42 @@
+#!/usr/bin/ruby
+# require 'pry'
 require 'digest'
 require 'net/http'
 
 class ScriptUpdater
-  REMOTE_SCRIPT_URL = 'https://raw.githubusercontent.com/ariera/auto-updatable-script/master/auto-updatable-script.rb'
-  def initialize
-    local_script_content = File.read(__FILE__)
-    local_script_digest = Digest::SHA256.hexdigest(local_script_content)
-    remote_script_content = Net::HTTP.get(URI(REMOTE_SCRIPT_URL))
-    remote_script_digest = Digest::SHA256.hexdigest(remote_script_content)
+  attr_reader :local_script_content, :local_script_digest, :remote_script_content, :remote_script_digest
 
-    if remote_script_digest != local_script_digest
-      puts "script needs update"
-      File.open(__FILE__, 'w') do |file|
-        file.puts remote_script_content
-      end
-    else
-      puts "no update needed"
+  def initialize(remote_script_url)
+    @local_script_content = File.read(__FILE__)
+    @local_script_digest = Digest::SHA256.hexdigest(@local_script_content)
+    @remote_script_content = Net::HTTP.get(URI(remote_script_url)) rescue @local_script_content
+    @remote_script_digest = Digest::SHA256.hexdigest(@remote_script_content)
+  end
+
+  # returns a boolean indicating wether the script was updated or not
+  def call
+    return false unless new_version_available?
+    puts "script needs update"
+    File.open(__FILE__, 'w') do |file|
+      file.puts remote_script_content
     end
+    puts "updated :o)"
+    true
+  rescue
+    puts "rescued!!"
+    false
+  end
+
+  def new_version_available?
+    remote_script_digest != local_script_digest 
   end
 end
 
-ScriptUpdater.new
-puts "done :D"
+remote_script_url = 'https://raw.githubusercontent.com/ariera/auto-updatable-script/master/auto-updatable-script.rb'
+if ScriptUpdater.new(remote_script_url).call
+  puts "Script updated. Execution halted, please run the script again"
+  exit
+end
+
+## Your code goes down here
+puts "the script didn't an update so it will continue its execution as normal"
